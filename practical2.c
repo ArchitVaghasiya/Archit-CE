26a27494c370a6d6b125c875706a2e08a01dd015
ArchitVaghasiya <architvaghasiya231819@gmail.com>
2024-07-22 22:08:16 +0530

    Add files via upload

---
+++ practical2.c
@@ -0,0 +1,16 @@
+/*
+	programmer name: Archit Vaghasiya
+	date: 22/7/2024
+	aim: Find simple intrest
+*/ 
+#include<stdio.h>
+void main()
+{
+	int t;
+	float p,r,i;
+	p=50000;
+	t=3;
+	r=5.5;
+	i=(p*t*r)/100;
+	printf("i=%.2f",i);
+}
