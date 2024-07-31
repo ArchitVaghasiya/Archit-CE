2239f52744bc98e0dc080339b1da8b768f983ef9
ArchitVaghasiya <architvaghasiya231819@gmail.com>
2024-07-31 17:29:48 +0530

    Add files via upload

---
+++ practical9.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+void main()
+{
+	int Marks;
+	
+	printf("Enter your Marks :\t");
+	scanf("%d",&Marks);
+	
+	
+	if(Marks<=100 && Marks>=90)
+	{
+		printf("You enterd vailde Marks\n");
+		printf("You got A grade");
+	}
+	
+	else if(Marks>=80 && Marks<90)
+	{
+		printf("You got B grade");
+	}
+	
+	else if(Marks>=70 && Marks<80)
+	{
+		printf("You got C grade");
+	}
+	
+	else if(Marks>=60 && Marks<70)
+	{
+		printf("You got D grade");
+	}
+	
+	else if(Marks<60)
+	{
+		printf("You got F grade");
+	}
+	
+	else
+	{
+		
+		printf("you entered invalid marks\n");
+	
+	}
+
+}
