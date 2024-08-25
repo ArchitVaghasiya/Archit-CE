2320cb822b6e9d02ca2affc592427a20664aa19f
ArchitVaghasiya <architvaghasiya231819@gmail.com>
2024-08-25 22:49:03 +0530

    Update practical14C.c

--- practical14C.c
+++ practical14C.c
@@ -1,31 +1,35 @@
+/*
+    programmer: Archit Vaghasiya
+    date: 25/8/2024
+    aim: make a alphabet piramid
+*/
 #include<stdio.h>
 void main()
 {
-	int i,j,n;
-	char x;
-	
-	printf("Enter the value of n:");
-	scanf("%d",&n);
-	
-	for(i=1;i<=n;i++)
-	{
-		for(j=1;j<=(n-i+1);j++)
-		{
-			printf(" ");
-		}
-		x='A';
-		for(j=(n-i+1);j<=n;j++)
-		{
-			printf("%c",x);
-			x++;
-		}
-		x='A';
-		for(j=2;j<=i;j++)
-		{
-			printf("%c",x);
-			x++;
-		}
-		printf("\n");
-	}
-		
+    int i,j,n;
+    char x;
+
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=(n-i+1);j++)
+        {
+            printf(" ");
+        }
+        x='A';
+        for(j=1;j<=i;j++)
+        {
+            printf("%c",x);
+            x++;
+        }
+        x-=2;
+        for(j=(n+1);j<=(n+i-1);j++)
+        {
+            printf("%c",x);
+            --x;
+        }
+        printf("\n");
+    }
 }
