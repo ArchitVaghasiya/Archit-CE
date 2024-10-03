6eae83cae38c6164233f5251d7cf03d6c3b6e998
ArchitVaghasiya <architvaghasiya231819@gmail.com>
2024-10-03 15:59:19 +0530

    Add files via upload

---
+++ practical22.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int S[25],T,n,H,L,i,j=1,c;
+    float A;
+
+    printf("Enter the number of student:");
+    scanf("%d",&n);
+    printf("Enter CCP markes of students\n");
+    for(i=0;i<n;i++)
+    {
+        printf("Student %d:",j++);
+        scanf("%d",&S[i]);
+        c++
+    }
+
+    for
+}
